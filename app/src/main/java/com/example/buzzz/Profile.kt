a2aadaf7cfb495bad8819df7a3343c690d82e23a
pritishkr <pritish.kr3@gmail.com>
2022-01-22 11:27:58 +0530

    Commit @22ndjan

--- app/src/main/java/com/example/buzzz/Profile.kt
+++ app/src/main/java/com/example/buzzz/Profile.kt
@@ -1,11 +1,23 @@
 package com.example.buzzz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.buzzz.databinding.ActivityProfileBinding
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class Profile : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        button_continue.setOnClickListener{
+            val intent = Intent(this,Bio::class.java)
+            startActivity(intent)
+        }
     }
 }
