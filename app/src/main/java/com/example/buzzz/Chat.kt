a3050f0ea3edfc75f875ab80d40d1347d596f13c
pritishkr <pritish.kr3@gmail.com>
2022-03-03 01:24:53 +0530

    commit@3rd March

--- app/src/main/java/com/example/buzzz/Chat.kt
+++ app/src/main/java/com/example/buzzz/Chat.kt
@@ -1,11 +1,18 @@
 package com.example.buzzz
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.buzzz.databinding.ActivityChatBinding
+import com.example.buzzz.databinding.ActivitySplashBinding
 
 class Chat : AppCompatActivity() {
+    private lateinit var binding: ActivityChatBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
+        binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
